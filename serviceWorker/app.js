af4864f7817fffbb67485ce64c278ec3a54acf6b
朱羽佳 <zhuyujia@qiyi.com>
2020-03-02 14:56:21 +0800

    +

---
+++ serviceWorker/app.js
@@ -0,0 +1,15 @@
+if ('serviceWorker' in navigator) {
+    window.addEventListener('load', () => {
+        navigator.serviceWorker.register('sw.js', {scope: '/serviceWork'}).then(reg => {
+            if(reg.installing) {
+                console.log('Service worker installing');
+            } else if(reg.waiting) {
+                console.log('Service worker installed');
+            } else if(reg.active) {
+                console.log('Service worker active');
+            }
+        }).catch(err => {
+            console.log('Registration failed with ' + err);
+        });
+    });
+}
