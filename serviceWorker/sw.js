7a824e2b9bee4820ef6264321d32a28856eed6da
朱羽佳 <zhuyujia@qiyi.com>
2020-03-03 15:10:26 +0800

    +

--- serviceWorker/sw.js
+++ serviceWorker/sw.js
@@ -34,29 +34,10 @@ self.addEventListener('activate', event => {
 });
   
 self.addEventListener('fetch', event => {
-    console.log(event.request, event.request.url);
     event.respondWith(
         caches.match(event.request).then(response => {
-            // caches.match() always resolves
-            // but in case of success response will have value
-            // if (response !== undefined) {
-            //     return response;
-            // } else {
-            //     return fetch(event.request).then(function (response) {
-            //         // response may be used only once
-            //         // we need to save clone to put one copy in cache
-            //         // and serve second one
-            //         let responseClone = response.clone();
-                    
-            //         caches.open('v1').then(function (cache) {
-            //             cache.put(event.request, responseClone);
-            //         });
-            //         return response;
-            //     }).catch(function () {
-            //         return caches.match('/sw-test/gallery/myLittleVader.jpg');
-            //     });
-            // }
-            return response || fetch(event.request);
+            if (response) return response;
+            // return response || fetch(event.request);
         }).catch(err => {
             console.log(err);
         })
